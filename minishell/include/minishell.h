1960e036367dc37e44c901e546b65f9aa806ca27
Alfonso Sánchez <alfonso1799@gmail.com>
2025-05-15 18:32:10 +0200

    First commit minishell

---
+++ minishell/include/minishell.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alfsanch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 19:03:23 by alfsanch          #+#    #+#             */
+/*   Updated: 2024/07/17 17:05:05 by alfsanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <sys/wait.h>
+# include <limits.h>
+# include <errno.h>
+# include <signal.h>
+
+
+# define GREEN    "\033[32m"
+# define RESET    "\033[0m"
+# define RED      "\033[31m"
+
+#endif
