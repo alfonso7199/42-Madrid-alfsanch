94ebc0eb4e63c4d4253ef404ee565c5078547ba8
alfonso7199 <alfonso1799@gmail.com>
2025-05-22 12:04:47 +0200

    init some files

--- minishell/include/minishell.h
+++ minishell/include/minishell.h
@@ -14,20 +14,63 @@
 # define MINISHELL_H
 
 # include "../libft/libft.h"
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <string.h>
-# include <fcntl.h>
 # include <dirent.h>
-# include <sys/wait.h>
-# include <limits.h>
 # include <errno.h>
+# include <fcntl.h>
+# include <libgen.h>
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <signal.h>
-
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/ioctl.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <termios.h>
+# include <unistd.h>
 
 # define GREEN    "\033[32m"
 # define RESET    "\033[0m"
 # define RED      "\033[31m"
+
+typedef enum e_token_type {
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT,
+	TOKEN_APPEND,
+	TOKEN_HEREDOC,
+	TOKEN_EOF
+} t_token_type;
+
+typedef struct s_token {
+    t_token_type   type;
+    char          *value;
+    struct s_token *next;
+} t_token;
+
+typedef struct s_redir {
+    t_token_type   type;
+    char          *file;
+    struct s_redir *next;
+} t_redir;
+
+typedef struct s_cmd {
+    char         **args;
+    t_redir      *redirections;
+    struct s_cmd *next;
+} t_cmd;
+
+t_token *tokenizer(char *input);
+t_cmd   *parser(t_token *tokens);
+
+
+/* Errores */
+int     error_msg(char *msg);
+int     ft_error(int error, t_tools *tools);
+void    reset_tools(t_tools *tools);
 
 #endif
