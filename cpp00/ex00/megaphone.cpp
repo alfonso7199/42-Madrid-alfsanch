b5543bf01355d5948baf0ea3e4e2a8a737197a71
Alfonso Sánchez <alfonso1799@gmail.com>
2025-08-05 19:12:52 +0200

    ex00 complete

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -9,3 +9,23 @@
 /*   Updated: 2024/07/17 17:05:05 by alfsanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+
+	j = -1;
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while (argc > 1 && argv && argv[++j])
+	{
+		i = -1;
+		while (j > 0 && argv[j][++i])
+			std::cout << (char)std::toupper(argv[j][i]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
