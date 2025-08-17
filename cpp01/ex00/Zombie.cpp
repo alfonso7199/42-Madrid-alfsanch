93dfa1fbf4ed8c4b2d88e4df7ba3f2b03af3059e
Alfonso Sánchez <alfonso1799@gmail.com>
2025-08-17 20:10:01 +0200

    cpp 01 ex00

---
+++ cpp01/ex00/Zombie.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alfsanch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 19:03:23 by alfsanch          #+#    #+#             */
+/*   Updated: 2024/07/17 17:05:05 by alfsanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) : name(name)
+{
+    std::cout << "Zombie object " << name << " created" << std::endl;
+}
+
+void Zombie::announce() const
+{
+    std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::~Zombie()
+{
+    std::cout << "Zombie object " << name << " destroyed" << std::endl;
+}
