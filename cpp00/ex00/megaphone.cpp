9ea6fc3b7d7ed235e0b77653d17f1a9503aae21f
Alfonso Sánchez <alfonso1799@gmail.com>
2025-11-14 12:10:56 +0100

    clear megaphone version

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -17,14 +17,18 @@ int	main(int argc, char **argv)
 	int		i;
 	int		j;
 
-	j = -1;
+	j = 1;
 	if (argc < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	while (argc > 1 && argv && argv[++j])
+	while (argc > 1 && j < argc)
 	{
-		i = -1;
-		while (j > 0 && argv[j][++i])
-			std::cout << (char)std::toupper(argv[j][i]);
+		i = 0;
+        while (argv[j][i])
+        {
+            std::cout << (char)std::toupper(argv[j][i]);
+            i++;
+        }
+        j++;
 	}
 	std::cout << std::endl;
 	return (0);
