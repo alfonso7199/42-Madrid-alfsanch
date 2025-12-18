38eacf4d4d4e28376c6a213cd1e71f1f1f328ca8
Alfonso Sánchez <alfonso1799@gmail.com>
2025-12-18 12:44:44 +0100

    Fix cpp02

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -89,18 +89,18 @@ Fixed	Fixed::operator-(Fixed const &other) const {
 
 Fixed	Fixed::operator*(Fixed const &other) const {
 	Fixed	ret;
-	ret.setRawBits(getRawBits() * other.getRawBits() >> _fract);
+	ret.setRawBits(getRawBits() * other.getRawBits() >> fractionalBits);
 	return ret;
 }
 
 Fixed	Fixed::operator/(Fixed const &other) const {
 	Fixed	ret;
-	ret.setRawBits(getRawBits() / other.getRawBits() << _fract);
+	ret.setRawBits(getRawBits() / other.getRawBits() << fractionalBits);
 	return ret;
 }
 
 Fixed&	Fixed::operator++() {
-	_value++;
+	fixedPointNum++;
 	return *this;
 }
 
@@ -111,7 +111,7 @@ Fixed	Fixed::operator++(int) {
 }
 
 Fixed&	Fixed::operator--() {
-	_value--;
+	fixedPointNum--;
 	return *this;
 }
 
@@ -125,14 +125,14 @@ Fixed const &Fixed::min(Fixed const &a, Fixed const &b) {
 	return a < b ? a : b;
 }
 
+Fixed &Fixed::min(Fixed &a, Fixed &b) {
+	return a < b ? a : b;
+}
+
 Fixed const &Fixed::max(Fixed const &a, Fixed const &b) {
 	return a > b ? a : b;
 }
 
-Fixed const &min(Fixed const &a, Fixed const &b) {
-	return Fixed::min(a, b);
-}
-
-Fixed const &max(Fixed const &a, Fixed const &b) {
-	return Fixed::max(a, b);
+Fixed &Fixed::max(Fixed &a, Fixed &b) {
+	return a > b ? a : b;
 }
--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -34,13 +34,15 @@ class Fixed {
 		Fixed operator * (const Fixed &other) const;
 		Fixed operator / (const Fixed &other) const;
 
-		Fixed operator ++ (void);
+		Fixed& operator ++ (void);
 		Fixed operator ++ (int);
-		Fixed operator -- (void);
+		Fixed& operator -- (void);
 		Fixed operator -- (int);
 
-        const static Fixed	&min(Fixed const &a, Fixed const &b);
-		const static Fixed	&max(Fixed const &a, Fixed const &b);
+		static Fixed &min(Fixed &a, Fixed &b);
+		static Fixed const &min(Fixed const &a, Fixed const &b);
+		static Fixed &max(Fixed &a, Fixed &b);
+		static Fixed const &max(Fixed const &a, Fixed const &b);
 };
 
 std::ostream& operator<<(std::ostream &o, Fixed const &fixed);
